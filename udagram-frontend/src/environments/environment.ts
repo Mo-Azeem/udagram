ecd56c2d7d3ac134cacfab4c4a3da12248dc8fe2
MoeIsLearning <moeislearning@gmail.com>
2022-04-06 03:52:44 +0200

    k'

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-env.eba-gqekmc95.eu-central-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*
